100c919718175718a4c5181ae97a786e1fd956d1
ItzRabbs <maxim.rabinovich1@gmail.com>
2020-08-09 23:41:09 -0400

    updated setup.py and readme

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyPokerEngine',
-    version='1.0.2',
-    author='ishikota',
-    author_email='ishikota086@gmail.com',
-    description='Poker engine for poker AI development in Python ',
+    version='1.0.3',
+    author='ItzRabbs',
+    description='Poker engine for poker AI development in Python',
     license='MIT',
     keywords='python poker emgine ai',
-    url='https://github.com/ishikota/PyPokerEngine',
+    url='https://github.com/ItzRabbs/PyPokerEngine',
     packages=[pkg for pkg in find_packages() if pkg != "tests"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
